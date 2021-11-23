4474e3b4020ae22f06a5cc211e4cc8bbeff5bef2
Hendrik Heinle <hendrik.heinle@fluxel.de>
2021-11-23 13:18:27 +0100

    #1 route viewmodel propertychanged to controller functions

--- Presentation/Controller/ControllerBase.cs
+++ Presentation/Controller/ControllerBase.cs
@@ -1,12 +1,54 @@
-﻿using FluxWork.Presentation.ViewModel;
+﻿using System;
+using System.ComponentModel;
+using FluxWork.Presentation.ViewModel;
 using LightInject;
 
 namespace FluxWork.Presentation.Controller
 {
   public abstract class ControllerBase<TViewModel> where TViewModel : IViewModel
   {
+    private TViewModel _viewModel;
+    private event PropertyChangedEventHandler ViewModelPropertyChanged;
+
+    protected ControllerBase()
+    {
+      this.ViewModelPropertyChanged += OnViewModelPropertyChanged;
+    }
+
+    private void OnViewModelPropertyChanged(object sender, PropertyChangedEventArgs e)
+    {
+      var type = this.GetType();
+      var viewModelType = typeof(TViewModel);
+      
+      var propertyInfo = viewModelType.GetProperty(e.PropertyName);
+      if (propertyInfo == null)
+      {
+        return;
+      }
+
+      var methodInfo = type.GetMethod($"On{e.PropertyName}Changed", new[] {propertyInfo.PropertyType});
+      if (methodInfo == null)
+      {
+        return;
+      }
+
+      methodInfo.Invoke(this, new[] {propertyInfo.GetValue(this._viewModel)});
+    }
+
     [Inject]
-    public TViewModel ViewModel { get; set; }
+    public TViewModel ViewModel
+    {
+      get => _viewModel;
+      set
+      {
+        if (_viewModel != null)
+        {
+          _viewModel.PropertyChanged -= this.ViewModelPropertyChanged;
+        }
+        _viewModel = value;
+        _viewModel.PropertyChanged += this.ViewModelPropertyChanged;
+      }
+    }
 
     public virtual void Initialized()
     {
